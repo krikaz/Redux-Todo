7be21d2f980bfe5f8431c3547700797c332c2e93
krikaz <pascal.bou@protonmail.com>
2019-06-18 13:38:05 +0200

    created actions types and built on reducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// step 1 - only one state - todos arrays
+
+// step 2 - action types
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED';
+
+// STEP 3: BUILD ONE REDUCER PER STATE SLICE
+function todosReducer (state = {todos: []}, action) {
+  switch (action.type) {
+    case (ADD_TODO):
+      return [...state.todos, action.payload]; 
+    case (TOGGLE_COMPLETED):
+      return state.todos.map(todo => {
+        if (todo.value === action.payload) {
+          return {...todo, completed: true}
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
